541e522e68bacff027abbb5d167b5c6d83c5504c
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-04-19 12:01:57 -0500

    complete albums_seeder

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,1 +1,8 @@
 use codeup_test_db;
+insert into albums (artist, name, release_date, sales, genre)
+values ('Michael Jackson', 'Thriller', '1982', '51.2', 'Pop, post-disco, funk, rock'),
+       ('Whitney Houston', 'The Bodyguard', '1992', '28.7', 'R&B, soul, pop, soundtrack'),
+       ('Pink Floyd', 'The Dark Side of the Moon', '1973', '24.8', 'Progressive rock'),
+       ('Eagles', 'Their Greatest Hits', '1976', '41.2', 'Country rock, soft rock, folk rock'),
+       ('Meat Loaf', 'Bat Out of Hell', '1977', '22', 'Hard rock, glam rock, progressive rock'),
+       ('Shania Twain', 'Rumours', '1977', '29.3', 'Soft rock');
