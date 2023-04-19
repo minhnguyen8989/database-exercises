7dbaa42beb379fce7e6dc58692b2e30fde26b5a5
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-04-19 13:50:24 -0500

    complete update exercise

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,1 +1,21 @@
 use codeup_test_db;
+
+-- Find all albums in your table
+select name from albums;
+update albums
+set sales = sales * 10;
+select sales from albums;
+
+-- Find all albums released before 1980
+select name from albums where release_date < 1980;
+update albums
+set release_date = release_date - 100
+where release_date < 1980;
+select release_date from albums;
+
+-- Find all albums by Michael Jackson
+select name from albums where artist = 'Michael Jackson';
+update albums
+set artist = 'Peter Jackson'
+where artist = 'Michael Jackson';
+select artist from albums;
