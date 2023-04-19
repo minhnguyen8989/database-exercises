b403f66a46ae66b319ad405de653f4e8e004b95f
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-04-19 12:21:58 -0500

    update albums_seeder and complete select_exercises, will continue after lunch

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -5,4 +5,7 @@ values ('Michael Jackson', 'Thriller', '1982', '51.2', 'Pop, post-disco, funk, r
        ('Pink Floyd', 'The Dark Side of the Moon', '1973', '24.8', 'Progressive rock'),
        ('Eagles', 'Their Greatest Hits', '1976', '41.2', 'Country rock, soft rock, folk rock'),
        ('Meat Loaf', 'Bat Out of Hell', '1977', '22', 'Hard rock, glam rock, progressive rock'),
-       ('Shania Twain', 'Rumours', '1977', '29.3', 'Soft rock');
+       ('Shania Twain', 'Rumours', '1977', '29.3', 'Soft rock'),
+       ('Pink Floyd', 'The Wall', '1979', '18.9', 'Progressive rock'),
+       ('The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', '1967', '18.3','Rock'),
+       ('Nirvana', 'Nevermind', '1991', '17.8', 'Grunge, alternative rock');
---
+++ select_exercises.sql
@@ -0,0 +1,19 @@
+use codeup_test_db;
+
+-- Find the name of all albums by Pink Floyd
+select * from albums where artist = 'Pink Floyd';
+
+-- Find the year Sgt. Peppers Lonely Hearts Club Band was released
+select release_date from albums where name 'Sgt. Peppers Lonely Hearts Club Band';
+
+-- Find the genre for Nevermind
+select genre from albums where name = 'Nevermind';
+
+-- Find which albums were released in the 1990s
+select name from albums where release_date between 1990 and 1999;
+
+-- Find which albums had less than 20 million cerified sales
+select name from albums where sales < 20.0;
+
+-- Find all albums with a genre of "rock"
+select name from albums where genre = 'rock';
