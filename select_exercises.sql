bf29362b49efc9e0b84f6d804e000d582c7b6de1
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-04-19 12:27:13 -0500

    update select exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,7 +4,7 @@ use codeup_test_db;
 select * from albums where artist = 'Pink Floyd';
 
 -- Find the year Sgt. Peppers Lonely Hearts Club Band was released
-select release_date from albums where name 'Sgt. Peppers Lonely Hearts Club Band';
+select release_date from albums where name = 'Sgt. Peppers Lonely Hearts Club Band';
 
 -- Find the genre for Nevermind
 select genre from albums where name = 'Nevermind';
@@ -16,4 +16,4 @@ select name from albums where release_date between 1990 and 1999;
 select name from albums where sales < 20.0;
 
 -- Find all albums with a genre of "rock"
-select name from albums where genre = 'rock';
+select name from albums where genre having 'rock';
