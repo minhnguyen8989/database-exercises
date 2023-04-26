7aec11fb6ea7c2fc7015d7ea0514b79981ad4d6d
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-04-26 15:07:55 -0500

    saving join exercises, will continue on later

---
+++ join_exercises.sql
@@ -0,0 +1,19 @@
+use employees;
+
+
+select departments.dept_name as Department_Name, concat(employees.first_name,' ', employees.last_name) as Department_Manager
+from employees
+join dept_manager on employees.emp_no = dept_manager.emp_no
+join departments on dept_manager.dept_no = departments.dept_no;
+
+select departments.dept_name as Department_Name, concat(employees.first_name,' ', employees.last_name) as Department_Manager
+from employees
+         join dept_manager on employees.emp_no = dept_manager.emp_no
+         join departments on dept_manager.dept_no = departments.dept_no
+where employees.gender = 'F';
+
+select titles.title, count
+
+
+
+
